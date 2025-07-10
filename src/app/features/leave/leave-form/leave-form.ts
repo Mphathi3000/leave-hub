f70d024e3062520a07165b46515a45debff6ede0
Asiphe Mtingane <ammtingane@Asiphes-MacBook-Pro.local>
2025-07-11 01:51:08 +0200

    Initial commit of LeaveHub project

---
+++ src/app/features/leave/leave-form/leave-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-form',
+  imports: [],
+  templateUrl: './leave-form.html',
+  styleUrl: './leave-form.scss'
+})
+export class LeaveForm {
+
+}
---
+++ src/app/features/leave/leave-history/leave-history.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-history',
+  imports: [],
+  templateUrl: './leave-history.html',
+  styleUrl: './leave-history.scss'
+})
+export class LeaveHistory {
+
+}
